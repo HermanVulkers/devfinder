d471f4faa7ed79aa14a23ad6d7eb63028e624fc0
hermanvulkers <hermanvulkers@gmail.com>
2021-12-08 15:25:29 +0100

    Added localStorage to dark mode

--- themeswitcher.js
+++ themeswitcher.js
@@ -1,11 +1,25 @@
 const btn = document.querySelector(".themeswitcher-btn");
 console.log(btn);
 
+const currentTheme = localStorage.getItem("currentTheme");
+if (currentTheme == "dark") {
+	document.body.classList.add("dark");
+} else {
+	document.body.classList.remove("dark");
+}
+
 function change() {
+	const dark = document.body.classList.contains("dark");
+	console.log(dark);
+
 	if (document.body.classList.contains("dark")) {
 		document.body.classList.remove("dark");
+		localStorage.setItem("currentTheme", "light");
+		console.log(localStorage);
 	} else {
 		document.body.classList.add("dark");
+		localStorage.setItem("currentTheme", "dark");
+		console.log(localStorage);
 	}
 }
 
